26d049c9573ad09d873a971ce4e9fb160e4d2d3d
sanya chawla <chawlasanya327@gmail.com>
2023-12-07 23:16:00 +0530

    trying to get number of reviews and ratings

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     brand_name = request.form['brand_name']
-    csv_filename = scrape_search_results(brand_name)
+    csv_filename = scrape_search_results(brand_name)  # Assuming scrape_search_results returns the filename
+    return render_template('index.html', csv_filename=csv_filename)
+
+@app.route('/download/<csv_filename>', methods=['GET'])
+def download(csv_filename):
     return send_file(csv_filename, as_attachment=True)
 
 if __name__ == '__main__':
